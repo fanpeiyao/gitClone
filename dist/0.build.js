844770d50129d6c93ceb97be411f5491b8e49467
Peiyao <952568789@qq.com>
2018-02-28 19:21:02 +0800

    Update 0.build.js

--- dist/0.build.js
+++ dist/0.build.js
@@ -1,36 +1,21 @@
-{
-  "name": "s3",
-  "version": "1.0.0",
-  "description": "gongyinju s3 tool box",
-  "license": "MIT",
-  "scripts":{
-    "test":"karma start test/unit/karma.config.js"
-  },
-  "devDependencies": {
-    "axios": "^0.17.1",
-    "chai": "^4.1.2",
-    "mocha": "^5.0.0",
-    "grunt": "^1.0.1",
-    "grunt-contrib-clean": "^1.1.0",
-    "grunt-contrib-compress": "^1.4.3",
-    "grunt-contrib-concat": "^1.0.1",
-    "grunt-contrib-copy": "^1.0.0",
-    "grunt-contrib-uglify": "^3.1.0",
-    "grunt-karma": "^2.0.0",
-    "jasmine-core": "^2.8.0",
-    "karma": "^2.0.0",
-    "karma-chrome-launcher": "^2.2.0",
-    "karma-coverage": "^1.1.1",
-    "karma-mocha": "^1.3.0",
-    "karma-chai": "^0.1.0",
-    "load-grunt-tasks": "^3.5.2",
-    "phantomjs-prebuilt": "^2.1.7",
-    "time-grunt": "^1.4.0"
-  },
-  "files": [
-    "dist",
-    "src/*.js",
-    "Gruntfile.js",
-    "LICENSE"
-  ]
+ Date.prototype.format = function(fmt) {
+     var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(!fmt) fmt = 'yyyy-MM-dd'
+    if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+     for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+         }
+     }
+    return fmt;
 }
