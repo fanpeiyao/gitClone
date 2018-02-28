63fd772cc0f76778e8fd804e0b30719e7377542c
Peiyao <952568789@qq.com>
2018-02-28 07:58:33 +0000

    Update 0.build.js

--- dist/0.build.js
+++ dist/0.build.js
@@ -1,22 +1,36 @@
-
-	 const node=document.createElement("input");
-    document.body.appendChild(node);
-    it("underscore_debounce", () => {
-
-        function testFn() {
-            console.log('333');
-        }
-        var input  = document.getElementsByTagName("INPUT")[1];
-        input.onkeyup =  s3.debounce(testFn, 300 );
-    });
-
-
-    it("underscore_throttle", () => {
-        let div=document.createElement("div"),count = 0;
-        document.body.appendChild(div);
-        var demo = document.getElementsByTagName("div")[0];
-        function underscore_throttle() {
-            demo.innerHTML += 'underscore_throttle 被调用了' + ++count +'次<br>';
-        }
-        window.onresize = s3.throttle(underscore_throttle,200)
-    });
+{
+  "name": "s3",
+  "version": "1.0.0",
+  "description": "gongyinju s3 tool box",
+  "license": "MIT",
+  "scripts":{
+    "test":"karma start test/unit/karma.config.js"
+  },
+  "devDependencies": {
+    "axios": "^0.17.1",
+    "chai": "^4.1.2",
+    "mocha": "^5.0.0",
+    "grunt": "^1.0.1",
+    "grunt-contrib-clean": "^1.1.0",
+    "grunt-contrib-compress": "^1.4.3",
+    "grunt-contrib-concat": "^1.0.1",
+    "grunt-contrib-copy": "^1.0.0",
+    "grunt-contrib-uglify": "^3.1.0",
+    "grunt-karma": "^2.0.0",
+    "jasmine-core": "^2.8.0",
+    "karma": "^2.0.0",
+    "karma-chrome-launcher": "^2.2.0",
+    "karma-coverage": "^1.1.1",
+    "karma-mocha": "^1.3.0",
+    "karma-chai": "^0.1.0",
+    "load-grunt-tasks": "^3.5.2",
+    "phantomjs-prebuilt": "^2.1.7",
+    "time-grunt": "^1.4.0"
+  },
+  "files": [
+    "dist",
+    "src/*.js",
+    "Gruntfile.js",
+    "LICENSE"
+  ]
+}
