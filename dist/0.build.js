886c7b28eb08f58b3bff0bf8242d232dd563b593
Peiyao <952568789@qq.com>
2018-04-18 10:37:53 +0800

    Update 0.build.js

--- dist/0.build.js
+++ dist/0.build.js
@@ -1,301 +1,134 @@
-<template>
-  <div :class="[
-    type === 'textarea' ? 'el-textarea' : 'el-input',
-    inputSize ? 'el-input--' + inputSize : '',
-    {
-      'is-disabled': inputDisabled,
-      'el-input-group': $slots.prepend || $slots.append,
-      'el-input-group--append': $slots.append,
-      'el-input-group--prepend': $slots.prepend,
-      'el-input--prefix': $slots.prefix || prefixIcon,
-      'el-input--suffix': $slots.suffix || suffixIcon
-    }
-    ]"
-    @mouseenter="hovering = true"
-    @mouseleave="hovering = false"
-  >
-    <template v-if="type !== 'textarea'">
-      <!-- 前置元素 -->
-      <div class="el-input-group__prepend" v-if="$slots.prepend">
-        <slot name="prepend"></slot>
-      </div>
-      <input
-        :tabindex="tabindex"
-        v-if="type !== 'textarea'"
-        class="el-input__inner"
-        v-bind="$attrs"
-        :type="type"
-        :disabled="inputDisabled"
-        :autocomplete="autoComplete"
-        :value="currentValue"
-        ref="input"
-        @compositionstart="handleComposition"
-        @compositionupdate="handleComposition"
-        @compositionend="handleComposition"
-        @input="handleInput"
-        @focus="handleFocus"
-        @blur="handleBlur"
-        @change="handleChange"
-        :aria-label="label"
-      >
-      <!-- 前置内容 -->
-      <span class="el-input__prefix" v-if="$slots.prefix || prefixIcon" :style="prefixOffset">
-        <slot name="prefix"></slot>
-        <i class="el-input__icon"
-           v-if="prefixIcon"
-           :class="prefixIcon">
-        </i>
-      </span>
-      <!-- 后置内容 -->
-      <span
-        class="el-input__suffix"
-        v-if="$slots.suffix || suffixIcon || showClear || validateState && needStatusIcon"
-        :style="suffixOffset">
-        <span class="el-input__suffix-inner">
-          <template v-if="!showClear">
-            <slot name="suffix"></slot>
-            <i class="el-input__icon"
-              v-if="suffixIcon"
-              :class="suffixIcon">
-            </i>
-          </template>
-          <i v-else
-            class="el-input__icon el-icon-circle-close el-input__clear"
-            @click="clear"
-          ></i>
-        </span>
-        <i class="el-input__icon"
-          v-if="validateState"
-          :class="['el-input__validateIcon', validateIcon]">
-        </i>
-      </span>
-      <!-- 后置元素 -->
-      <div class="el-input-group__append" v-if="$slots.append">
-        <slot name="append"></slot>
-      </div>
-    </template>
-    <textarea
-      v-else
-      :tabindex="tabindex"
-      class="el-textarea__inner"
-      :value="currentValue"
-      @compositionstart="handleComposition"
-      @compositionupdate="handleComposition"
-      @compositionend="handleComposition"
-      @input="handleInput"
-      ref="textarea"
-      v-bind="$attrs"
-      :disabled="inputDisabled"
-      :style="textareaStyle"
-      @focus="handleFocus"
-      @blur="handleBlur"
-      @change="handleChange"
-      :aria-label="label"
-    >
-    </textarea>
-  </div>
-</template>
-<script>
-  import emitter from 'element-ui/src/mixins/emitter';
-  import Migrating from 'element-ui/src/mixins/migrating';
-  import calcTextareaHeight from './calcTextareaHeight';
-  import merge from 'element-ui/src/utils/merge';
-  import { isKorean } from 'element-ui/src/utils/shared';
-  export default {
-    name: 'ElInput',
-    componentName: 'ElInput',
-    mixins: [emitter, Migrating],
-    inheritAttrs: false,
-    inject: {
-      elForm: {
-        default: ''
-      },
-      elFormItem: {
-        default: ''
-      }
-    },
-    data() {
-      return {
-        currentValue: this.value === undefined ? '' : this.value,
-        textareaCalcStyle: {},
-        prefixOffset: null,
-        suffixOffset: null,
-        hovering: false,
-        focused: false,
-        isOnComposition: false
-      };
-    },
-    props: {
-      value: [String, Number],
-      size: String,
-      resize: String,
-      form: String,
-      disabled: Boolean,
-      type: {
-        type: String,
-        default: 'text'
-      },
-      autosize: {
-        type: [Boolean, Object],
-        default: false
-      },
-      autoComplete: {
-        type: String,
-        default: 'off'
-      },
-      validateEvent: {
-        type: Boolean,
-        default: true
-      },
-      suffixIcon: String,
-      prefixIcon: String,
-      label: String,
-      clearable: {
-        type: Boolean,
-        default: false
-      },
-      tabindex: String
-    },
-    computed: {
-      _elFormItemSize() {
-        return (this.elFormItem || {}).elFormItemSize;
-      },
-      validateState() {
-        return this.elFormItem ? this.elFormItem.validateState : '';
-      },
-      needStatusIcon() {
-        return this.elForm ? this.elForm.statusIcon : false;
-      },
-      validateIcon() {
-        return {
-          validating: 'el-icon-loading',
-          success: 'el-icon-circle-check',
-          error: 'el-icon-circle-close'
-        }[this.validateState];
-      },
-      textareaStyle() {
-        return merge({}, this.textareaCalcStyle, { resize: this.resize });
-      },
-      inputSize() {
-        return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
-      },
-      inputDisabled() {
-        return this.disabled || (this.elForm || {}).disabled;
-      },
-      isGroup() {
-        return this.$slots.prepend || this.$slots.append;
-      },
-      showClear() {
-        return this.clearable && !this.disabled && this.currentValue !== '' && (this.focused || this.hovering);
-      }
-    },
-    watch: {
-      'value'(val, oldValue) {
-        this.setCurrentValue(val);
-      }
-    },
-    methods: {
-      focus() {
-        (this.$refs.input || this.$refs.textarea).focus();
-      },
-      blur() {
-        (this.$refs.input || this.$refs.textarea).blur();
-      },
-      getMigratingConfig() {
-        return {
-          props: {
-            'icon': 'icon is removed, use suffix-icon / prefix-icon instead.',
-            'on-icon-click': 'on-icon-click is removed.'
-          },
-          events: {
-            'click': 'click is removed.'
-          }
-        };
-      },
-      handleBlur(event) {
-        this.focused = false;
-        this.$emit('blur', event);
-        if (this.validateEvent) {
-          this.dispatch('ElFormItem', 'el.form.blur', [this.currentValue]);
-        }
-      },
-      select() {
-        (this.$refs.input || this.$refs.textarea).select();
-      },
-      resizeTextarea() {
-        if (this.$isServer) return;
-        const { autosize, type } = this;
-        if (type !== 'textarea') return;
-        if (!autosize) {
-          this.textareaCalcStyle = {
-            minHeight: calcTextareaHeight(this.$refs.textarea).minHeight
-          };
-          return;
-        }
-        const minRows = autosize.minRows;
-        const maxRows = autosize.maxRows;
-        this.textareaCalcStyle = calcTextareaHeight(this.$refs.textarea, minRows, maxRows);
-      },
-      handleFocus(event) {
-        this.focused = true;
-        this.$emit('focus', event);
-      },
-      handleComposition(event) {
-        if (event.type === 'compositionend') {
-          this.isOnComposition = false;
-          this.handleInput(event);
-        } else {
-          const text = event.target.value;
-          const lastCharacter = text[text.length - 1] || '';
-          this.isOnComposition = !isKorean(lastCharacter);
-        }
-      },
-      handleInput(event) {
-        if (this.isOnComposition) return;
-        const value = event.target.value;
-        this.$emit('input', value);
-        this.setCurrentValue(value);
-      },
-      handleChange(event) {
-        this.$emit('change', event.target.value);
-      },
-      setCurrentValue(value) {
-        if (value === this.currentValue) return;
-        this.$nextTick(_ => {
-          this.resizeTextarea();
-        });
-        this.currentValue = value;
-        if (this.validateEvent) {
-          this.dispatch('ElFormItem', 'el.form.change', [value]);
-        }
-      },
-      calcIconOffset(place) {
-        const pendantMap = {
-          'suf': 'append',
-          'pre': 'prepend'
-        };
-        const pendant = pendantMap[place];
-        if (this.$slots[pendant]) {
-          return { transform: `translateX(${place === 'suf' ? '-' : ''}${this.$el.querySelector(`.el-input-group__${pendant}`).offsetWidth}px)` };
-        }
-      },
-      clear() {
-        this.$emit('input', '');
-        this.$emit('change', '');
-        this.$emit('clear');
-        this.setCurrentValue('');
-        this.focus();
-      }
-    },
-    created() {
-      this.$on('inputSelect', this.select);
-    },
-    mounted() {
-      this.resizeTextarea();
-      if (this.isGroup) {
-        this.prefixOffset = this.calcIconOffset('pre');
-        this.suffixOffset = this.calcIconOffset('suf');
-      }
-    }
-  };
-</script>
+.el-input {
+    position: relative;
+    font-size: 14px;
+    display: inline-block;
+    width: 100%;
+}
+.el-input__inner {
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 100%;
+}
+.el-input.is-disabled .el-input__inner {
+    background-color: #f5f7fa;
+    border-color: #e4e7ed;
+    color: #c0c4cc;
+    cursor: not-allowed;
+}
+.el-input--suffix .el-input__inner {
+    padding-right: 30px;
+}
+.el-input__suffix {
+    position: absolute;
+    height: 100%;
+    right: 5px;
+    top: 0;
+    text-align: center;
+    color: #c0c4cc;
+    transition: all .3s;
+    pointer-events: none;
+}
+.el-input__suffix-inner {
+    pointer-events: all;
+}
+.el-input__icon, .el-input__prefix {
+    height: 100%;
+    text-align: center;
+    transition: all .3s;
+}
+.el-input__icon {
+    width: 25px;
+    line-height: 40px;
+}
+.el-input__prefix {
+    position: absolute;
+    left: 5px;
+    top: 0;
+    color: #c0c4cc;
+}
+.el-textarea {
+    display: inline-block;
+    width: 100%;
+    vertical-align: bottom;
+    font-size: 14px;
+}
+.el-textarea__inner {
+    display: block;
+    resize: vertical;
+    padding: 5px 15px;
+    line-height: 1.5;
+    box-sizing: border-box;
+    width: 100%;
+    font-size: inherit;
+    color: #606266;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #dcdfe6;
+    border-radius: 4px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+}
+.el-input-group__prepend {
+    border-right: 0;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.el-input-group--prepend .el-input__inner, .el-input-group__append {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+.el-input-group__append {
+    border-left: 0;
+}
+.el-input-group__append button.el-button, .el-input-group__append div.el-select .el-input__inner, .el-input-group__append div.el-select:hover .el-input__inner, .el-input-group__prepend button.el-button, .el-input-group__prepend div.el-select .el-input__inner, .el-input-group__prepend div.el-select:hover .el-input__inner {
+    border-color: transparent;
+    background-color: transparent;
+    color: inherit;
+    border-top: 0;
+    border-bottom: 0;
+}
+.el-input__suffix {
+    position: absolute;
+    height: 100%;
+    right: 5px;
+    top: 0;
+    text-align: center;
+    color: #c0c4cc;
+    transition: all .3s;
+    pointer-events: none;
+}
+.el-input__suffix-inner {
+    pointer-events: all;
+}
+.el-select .el-input .el-select__caret {
+    color: #c0c4cc;
+    font-size: 14px;
+    transition: transform .3s;
+    transform: rotate(180deg);
+    line-height: 16px;
+    cursor: pointer;
+}
+.el-input--medium .el-input__inner {
+    height: 36px;
+    line-height: 36px;
+}
+.el-input--small .el-input__inner {
+    height: 32px;
+    line-height: 32px;
+}
+.el-input--mini .el-input__inner {
+    height: 28px;
+    line-height: 28px;
+}
+
