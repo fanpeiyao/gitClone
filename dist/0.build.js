eb1912b0eb2966919fa2e3125266d36e084a26bb
Peiyao <952568789@qq.com>
2018-04-28 02:26:40 +0000

    Update 0.build.js

--- dist/0.build.js
+++ dist/0.build.js
@@ -1,21 +1,55 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Example block-level help text here.</p>
-  </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Check me out
-    </label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+
+import { Filter,Directive,JSLoader,CSSLoader,CallbackHook } from './global';
+
+
+import s3Banner from './mobile/s3-banner.vue'
+import s3Bottom from './mobile/s3-bottom.vue'
+import s3Card from  './mobile/s3-card.vue'
+import s3Date from './mobile/s3-banner.vue'
+import s3Dealer from './mobile/s3-bottom.vue'
+import s3Firstlogin from  './mobile/s3-firstlogin.vue'
+import s3Formvalcode from './mobile/s3-formValCode.vue'
+import s3Functionpanel from './mobile/s3-functionPanel.vue'
+import s3Input from  './mobile/s3-input.vue'
+import s3InputNumber from  './mobile/s3-inputNumber.vue'
+import s3Login from './mobile/s3-login.vue'
+import s3Upload from './mobile/s3-upload.vue'
+import s3Header from  './mobile/s3-header.vue'
+
+
+const s3vue = function (Vue,options) {
+  //component
+  Vue.component(CSSLoader.name,CSSLoader)
+  Vue.component(JSLoader.name,JSLoader);
+  Vue.component(AsyncComponent.name,AsyncComponent)
+  Vue.component(s3Banner.name,s3Banner);
+  Vue.component(s3Bottom.name,s3Bottom);
+  Vue.component(s3Card.name,s3Card);
+  Vue.component(s3Date.name,s3Date);
+  Vue.component(s3Dealer.name,s3Dealer);
+  Vue.component(s3Firstlogin.name,s3Firstlogin);
+  Vue.component(s3Formvalcode.name,s3Formvalcode);
+  Vue.component(s3Functionpanel.name,s3Functionpanel);
+  Vue.component(s3Input.name,s3Input);
+  Vue.component(s3InputNumber.name,s3InputNumber);
+  Vue.component(s3Login.name,s3Login);
+  Vue.component(s3Upload.name,s3Upload);
+  Vue.component(s3Header.name,s3Header);
+
+
+  //prototype
+  // Vue.prototype.$http = s3.ajax
+  Vue.prototype.$hook = CallbackHook.run;
+
+  //install
+  Vue.use(Filter);
+  Vue.use(Directive)
+}
+
+if(typeof window !== 'undefined' && window.Vue){
+  s3vue(window.Vue)
+}
+
+module.exports = s3vue
+module.exports.default = s3vue
+
